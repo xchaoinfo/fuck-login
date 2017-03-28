32c9e094cb5199949e55606b155aab9c8c4cbee6
helm <ichaf@outlook.com>
2017-03-28 23:47:58 +0800

    fix weibo.cn login

--- 003 weibo.cn/weibo.cn.py
+++ 003 weibo.cn/weibo.cn.py
@@ -22,20 +22,21 @@ agent = 'Mozilla/5.0 (Windows NT 5.1; rv:33.0) Gecko/20100101 Firefox/33.0'
 
 headers = {
     "User-Agent": agent,
-    "Host": "login.weibo.cn",
+    "Host": "weibo.cn",
     "Origin": "https://login.weibo.cn",
     "Referer": "https://login.weibo.cn/login/"
 }
 
 session = requests.session()
 
-url_login = 'https://login.weibo.cn/login/'
+url_login = 'https://weibo.cn/login/'
 
 
 def get_params(url_login):
     html = session.get(url_login, headers=headers)
     # print(html.text)
-    pattern = r'action="(.*?)".*?type="password" name="(.*?)".*?name="vk" value="(.*?)".*?name="capId" value="(.*?)"'
+    pattern = r'action="(*?)".*?type="password" name="(.*?)".*?name="vk"\
+                 value="(.*?)".*?name="capId" value="(.*?)"'
     res = re.findall(pattern, html.text, re.S)
     # print(res)
     return res
@@ -54,10 +55,9 @@ def get_cha(capId):
         im.close()
     except:
         print("请到当前目下去找cha.jpg 输入验证码")
-    cha_code = input("请输入验证码")
+    cha_code = input("请输入验证码:")
 
     return cha_code
-
 
 
 res = get_params(url_login)
@@ -69,24 +69,25 @@ else:
 
 if __name__ == "__main__":
     cha_code = get_cha(capId)
-    email = input("请输入你的邮箱账号或者手机号码")
-    password_input = input("请输入你的密码")
+    email = input("请输入你的邮箱账号或者手机号码:")
+    password_input = input("请输入你的密码:")
     postdata = {
         "mobile": email,
         "code": cha_code,
         "remember": "on",
         "backURL": "http%3A%2F%2Fweibo.cn",
-        "backTitle": "手机新浪网",
+        "backTitle": "微博",
         "tryCount": "",
         "vk": vk,
         "capId": capId,
         "submit": "登录",
     }
+    # print(postdata)
     postdata[password] = password_input
     post_url = url_login + post_url
+    # print(post_url)
     page = session.post(post_url, data=postdata, headers=headers)
     index = session.get("http://weibo.cn")
     print(index.text)
-
-
-
+    # cookies = requests.utils.dict_from_cookiejar(session.cookies)
+    # print(cookies)
